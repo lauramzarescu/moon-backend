f4b5aa2ef4b837d0413998adeae6d00ada231087
Amzarescu Laurentiu <amzarescu41@gmail.com>
2025-03-21 14:47:12 +0200

    test

--- src/config/aws.config.ts
+++ src/config/aws.config.ts
@@ -1,26 +1,31 @@
 import dotenv from 'dotenv';
 import {EC2Client} from '@aws-sdk/client-ec2';
 import {ECSClient} from "@aws-sdk/client-ecs";
-import {fromContainerMetadata} from "@aws-sdk/credential-providers";
+import {fromInstanceMetadata} from "@aws-sdk/credential-providers";
 
 dotenv.config();
 
-// export const configuration = {
-//     region: process.env.AWS_REGION,
-//     credentials: {
-//         accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-//         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!
-//     },
-//     retryMode: 'standard',
-//     maxAttempts: 3,
-// };
-
-const configuration = {
-    credentials: fromContainerMetadata({
+let credentials;
+try {
+    credentials = fromInstanceMetadata({
         timeout: 1000,
         maxRetries: 0
     })
+} catch (error) {
+    // Fallback to environment variables
+    credentials = {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!
+    }
 }
+
+const configuration = {
+    region: process.env.AWS_REGION,
+    retryMode: 'standard',
+    maxAttempts: 3,
+    // credentials: credentials
+};
+
 
 export const ecsClient = new ECSClient(configuration);
 export const ec2Client = new EC2Client(configuration);
